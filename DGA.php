2ee3910fa8a7bc7719f21c51fe38a67947191bd2
userreport <userreport@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-26 12:53:50 +0000

    Plugin implemented
    git-svn-id: https://plugins.svn.wordpress.org/demographic-data-in-google-analytics/trunk@455911 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ DGA.php
@@ -0,0 +1,83 @@
+﻿<?php
+/*
+Plugin Name: Demographic data in Google Analytics
+Plugin URI:  http://www.userreport.com/userreport-wordpress-plugin/
+Description: A Plugin to add UserReport Analytics code on your pages, thus enabling the ability to transfer user data to Google Analytics. You simply enter your UserReport code snippet.
+Version: 1.0.2
+Author: UserReport
+Author URI: http://www.userreport.com
+*/
+
+	
+add_action('init', 'DGA_launch_i18n') ;
+add_action('admin_menu', 'DGA_menu') ;
+add_filter('plugin_action_links_'.plugin_basename(__FILE__), 'DGA_plugin_action_links') ;
+	
+function DGA_launch_i18n() {
+	$lngPath = basename(dirname(__FILE__)) . '/languages' ;
+	load_plugin_textdomain('DGA', false, $lngPath) ;
+}
+
+function DGA_menu() {
+	add_options_page('Demographic data in Google Analytics', __('DGA (UserReport)'), 'administrator', __FILE__, 'DGA_settings') ;
+	add_action( 'admin_init', 'DGA_register_mysettings') ;
+}
+	
+function DGA_plugin_action_links($links) {
+	$link = '<a href="' . menu_page_url ( plugin_basename(__FILE__) , false ) . '">' . __('Settings') .'</a>' ;
+	array_unshift($links, $link) ;
+	return $links ;
+}
+	
+function DGA_register_mysettings() {
+	register_setting('sga-settings-group', 'analytics_id') ;
+}
+	
+function DGA_settings() {
+
+	?>
+		<div class="wrap">
+			
+			<h2><?php _e('Demographic data in Google Analytics', 'DGA') ; ?></h2>
+			<p>
+				<?php
+					_e('This plugin allows you to easily add your UserReport code snippet to all your WordPress pages which will enable you to transfer background information about your visitors to google analytics. <br>If you do not have a code snippet, follow the 3 simple steps outlined below to get one:', 'DGA') ;
+					echo '<br/>' ;
+					_e('<li>Go to UserReport.com and create a free account</li><li>After your free UserReport account has been created, you will be asked to register your website (after completing this task, you will get a codesnippet).</li><li>Copy-paste the code snippet into the textbox below and save your changes.</li>', 'DGA') ;
+					echo '' ;
+					_e('', 'DGA') ;
+				?>
+			</p>
+	
+			<form method="post" action="options.php" id="google_form">
+				<?php settings_fields('sga-settings-group'); ?>
+		
+				<table class="form-table">
+					<tr valign="top">					
+					<textarea name="analytics_id" rows="2" cols="120"><?php echo get_option('analytics_id');?></textarea>
+					<br>Still unsure where to get the code snippet from? <a href="http://www.userreport.com/userreport-wordpress-plugin/" target="_blank">Click here</a> for more detailed instrictions.	
+					</tr>
+				</table>
+				<p class="submit"><input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" /></p>
+			</form>
+		
+			<p>
+				<?php
+					_e('<b>If you need any help?</b><br>Please <a href="http://www.userreport.com/support-and-news/forum/#/categories/wordpress-plugin" target="_blank">go to our friendly support forum</a>', 'DGA') ;
+					echo '<br/><br/>' ,
+						 '' ,
+						 '' ;
+					_e('', 'simple_google_analytics') ;
+					echo '<br/><br/>' ;
+					_e('', 'simple_google_analytics') ;
+					echo '<br/>' ;
+				?>
+			</p>
+		</div>
+	<?php
+}
+	
+	add_action('wp_head', 'DGA_function');
+
+	function DGA_function() {echo get_option('analytics_id');}
+?>
---
+++ readme.txt
@@ -0,0 +1,51 @@
+=== Demographic data in Google Analytics ===
+Author: userreport
+Contributors: userreport
+Tags: Analytics, Google, javascript, google analytics, custom variables, UserReport
+Plugin link: http://www.userreport.com/userreport-wordpress-plugin/
+Requires at least: 2.7
+Tested up to: 3.1.1
+Version: 1.0.0
+Stable tag: 1.0.0
+
+== Description ==
+This plugin adds the required javascript for the UserReport-service which will enable you to transfer background information about your visitors to google analytics.
+
+'Demographic data in Google Analytics' supports transfering up to 5 custom variables into google analytics. The google analytics custom variables can contain information such as: Gender, Age, Education and much more. The demografic data tranfered to google analytics is collected using the free UserReport service.
+
+This plugin does not provide the tracking code for Google Analytics. For that you will need to use a plugin like [Google Analytics for WordPress](http://wordpress.org/extend/plugins/google-analytics-for-wordpress/).
+
+For more information, visit the google analytics app gallery post [Get demographic data in Google Analytics with UserReport](http://www.google.com/analytics/apps/about?app_id=1174001) where you can learn more about the service
+
+Or visit the [Wordpress plugin homepage on UserReport](http://www.userreport.com/userreport-wordpress-plugin/)
+
+== Installation ==
+This section describes how to install the plugin:
+
+1. Upload the 'DGA.zip' file to the `/wp-content/plugins/` directory.
+2. Unzip the 'DGA.zip' which will create the folder to the directory `/wp-content/plugins/DGA`
+3. Activate the plugin through the 'Plugins' menu in WordPress
+4. Configure the plugin through 'DGA (UserReport)' submenu in the the 'Settings' section of the Wordpress administrator menu
+5. Add your UserReport code snippet 
+6. Save
+
+In other words, just follow the [standard WordPress installation method](http://codex.wordpress.org/Managing_Plugins#Installing_Plugins)
+
+== Frequently Asked Questions ==
+
+= Where can I find the Userreport code snippet? =
+Login to userreport.com and go to settings. Select Website-details and copy-paste the snippet from here
+
+= Can I get any support using this plugin? =
+Yes, we have created a support forum to help answers any questions relating to this plugin
+
+[Click here to go to our Wordpress Plugin support forum](http://www.userreport.com/support-and-news/forum/#/categories/wordpress-plugin)
+
+== Screenshots ==
+1. Screenshot of the custom variables in Google Analytics
+2. Screenshot of the custom variables in Google Analytics
+3. Screenshot of the basic settings panel for this plugin
+
+== Changelog ==
+= version 1.0.0 =
+* First released version
